83c8abdc4d8c5207126d197d1614a375c5e49099
peder nylander <peder@pedernylander.se>
2024-05-07 20:42:53 +0200

    first page without flag

--- src/app/start/page.tsx
+++ src/app/start/page.tsx
@@ -2,7 +2,9 @@ import React from "react";
 
 const Start = () => {
   return <div className="subPage">
-
+    <div className="text-6xl homePage">
+      Ski-vm solidaritet med Ukraina
+    </div>
   </div>
 };
 
