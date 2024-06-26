ebf477d1fc63d4309d58e41cd02e3e92340d816b
peder nylander <peder@pedernylander.se>
2024-06-26 14:42:01 +0200

    social worki9ng

--- src/Components/social/page.tsx
+++ src/Components/social/page.tsx
@@ -4,10 +4,10 @@ import Link from 'next/link';
 const Social = () => {
     return (
         <div className='social'>
-            <Link href="https://www.facebook.com/profile.php?id=61560288061445">
-            <span target="_blank" aria-label="Facebook" style={{ cursor: 'pointer', color: '#4267B2', display: 'inline-flex', alignItems: 'center' }}>
+          <Link href="https://www.facebook.com/profile.php?id=61560288061445" passHref legacyBehavior>
+            <a target="_blank" aria-label="Facebook" style={{ cursor: 'pointer', color: '#4267B2', display: 'inline-flex', alignItems: 'center' }}>
                 <FaFacebook size={30} />
-            </span>
+            </a>
         </Link>
         </div>
     );
